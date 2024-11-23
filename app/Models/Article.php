48b7169f34868573567e6aa1e988d04ff221b14d
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-23 14:52:44 +0200

    create Article model

---
+++ app/Models/Article.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title', 'content', 'news_id', 'user_id', 'attachment_path'
+    ];
+
+    public function news(): BelongsTo
+    {
+        return $this->belongsTo(News::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}

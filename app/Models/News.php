dcf0e3cad3463c386047f9e9b2ae665f5d8950bb
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-23 14:49:37 +0200

    create News model

---
+++ app/Models/News.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class News extends Model
+{
+    protected $fillable = [
+        'title', 'content', 'user_id', 'age_restriction', 'attachment_path'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
+}
